be4c7cd6093b7ee5c40fa58f9ab35e0628a4145d
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-24 00:37:41 -0400

    Add files via upload

--- navigator/GetLeaderboard.py
+++ navigator/GetLeaderboard.py
@@ -1,14 +1,16 @@
 import os,sys,json,base64,certifi
 from cryptography.fernet import Fernet
+from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
+
+load_dotenv(os.path.join(os.getcwd(),'.env'))
 
 database = MongoClient((Fernet(base64.b64decode(os.getenv('DBKEY'))).decrypt(os.getenv('DBTOKEN'))).decode(),tlsCAFile=certifi.where())
 collection = database["navigator"]["leaderboard"]
-
+leaderBoard = []
 leaders = list(collection.find().sort('longestRun', -1).limit(20))
 database.close()
-leaderBoard = []
-for leaderIndex in range(len(leaders)-1):
+for leaderIndex in range(len(leaders)):
     leader = leaders[leaderIndex]
     leaderBoard.append( {'name':leader['name'], 'time':leader['longestRun'], 'score':leader['highScore']} )
 
