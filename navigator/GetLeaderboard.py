a8bdff2fb4e51a5979189fd3e51c4e8610a23634
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-23 21:41:58 -0400

    Update GetLeaderboard.py

--- navigator/GetLeaderboard.py
+++ navigator/GetLeaderboard.py
@@ -7,10 +7,9 @@ load_dotenv(os.path.join(os.getcwd(),'.env'))
 
 database = MongoClient((Fernet(base64.b64decode(os.getenv('DBKEY'))).decrypt(os.getenv('DBTOKEN'))).decode(),tlsCAFile=certifi.where())
 collection = database["navigator"]["leaderboard"]
-database.close()
 leaderBoard = []
 leaders = list(collection.find().sort('longestRun', -1).limit(20))
-
+database.close()
 for leaderIndex in range(len(leaders)-1):
     leader = leaders[leaderIndex]
     leaderBoard.append( {'name':leader['name'], 'time':leader['longestRun'], 'score':leader['highScore']} )
